4abb3ff05210e704f6ca280e1516ca7d0f0f0deb
khiêm bá <bakhjem@gmail.com>
2019-07-19 09:37:22 +0700

    add

---
+++ precache-manifest.fa7993f9bb9633ef02688bf557b19da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6787c00b3946f3228778",
+    "url": "/static/css/main.b8743bb2.chunk.css"
+  },
+  {
+    "revision": "6787c00b3946f3228778",
+    "url": "/static/js/main.6787c00b.chunk.js"
+  },
+  {
+    "revision": "a6e6b092151def75292d",
+    "url": "/static/css/1.63696034.chunk.css"
+  },
+  {
+    "revision": "a6e6b092151def75292d",
+    "url": "/static/js/1.a6e6b092.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e20a9724933c5c114a016cf7bbfbd6bd",
+    "url": "/index.html"
+  }
+];
